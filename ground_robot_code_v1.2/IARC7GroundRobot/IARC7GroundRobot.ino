4667489c7cd099795876009af4d6e26057679690
gbennett62 <gerard_bennett@yahoo.com>
2016-02-22 16:46:34 -0500

    code is similar to IARC website, the difference is that this code can send the robot in a circle and make it change direction on command

---
+++ ground_robot_code_v1.2/IARC7GroundRobot/IARC7GroundRobot.ino
@@ -0,0 +1,97 @@
+/*******************************************************************************
+ * 
+ *  Ground Robot Firmware, developed for IARC Mission 7
+ *
+ *  Developed under Contract to IARC Design Committee
+ *  
+ *  Author: Sterling Lewis Peet <sterling.peet@gatech.edu>
+ *  Date:   January 3, 2014
+ * 
+ *  All Rights Reserved, please contact the IARC Competition Committee for
+ *  permissions requests.
+ *
+ ******************************************************************************/
+
+#include <FiniteStateMachine.h>
+
+////////////////////////////////////////////////////////////////////////////////
+// Version Information
+const byte VERSION_MAJOR = 1;
+const byte VERSION_MINOR = 2;
+
+////////////////////////////////////////////////////////////////////////////////
+// Hardware Constants
+
+//const byte rxPin = 0;
+//const byte txPin = 1;
+const byte brcPin = 6;
+
+const byte targetSwitchPin = 7; // The x position is target
+const byte runSigPin = 8; // Go Button
+const byte waitSigPin = 2; // Stop Button
+const byte topTouchPin = 3; // Magnetic Sensors
+
+const byte greenLed = 9;
+const byte redLed = 10;
+
+// Design Configuration Constants
+const int robotSpeed = 330; // mm/s
+
+// Global Variables
+
+// Robot Behavior State Machine
+State Start =             State(chooseType);
+State ObstacleWait =      State(obsWaitStart, obsWait, nullFunc);
+State ObstacleRun =       State(obsRunStart, obsRun, coiStopMoving);
+State ObstacleCollision = State(obsCrashStart, obsCrash, nullFunc);
+State TargetWait =        State(trgtWaitStart, trgtWait, trgtWaitExit);
+State TargetRun =         State(trgtRunStart, trgtRun, nullFunc);
+State TrajectoryNoise =   State(vNoiseStart, vNoise, nullFunc);
+State Reverse =           State(vReverseStart, vReverse, nullFunc);
+State TargetCollision =   State(trgtCrashStart, trgtCrash, nullFunc);
+State TopTouch =          State(touchStart, touch, nullFunc);
+//New
+State CircleRun =         State(circRunStart, circRun, nullFunc);
+State InfRun =         State(infRunStart, infRun, nullFunc);
+
+FSM fsm = FSM(Start);     // Initialize state machine, set start state
+
+void setup() {
+  Serial.begin(38400);
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  
+  // Print out the Sketch and Version Info
+  Serial.println("IARC Mission 7 Ground Robot Firmware");
+  Serial.print("Release Version: ");
+  Serial.print(VERSION_MAJOR);
+  Serial.print(".");
+  Serial.println(VERSION_MINOR);
+  
+  initRC();
+
+  // if analog input pin 0 is unconnected, random analog
+  // noise will cause the call to randomSeed() to generate
+  // different seed numbers each time the sketch runs.
+  // randomSeed() will then shuffle the random function.
+  randomSeed(analogRead(0));
+
+  // Indicate initialization mode
+  digitalWrite(greenLed, HIGH);
+  digitalWrite(redLed, HIGH);
+
+  coiInit();
+  coiSafeMode();
+  delay(200);
+
+  // Stop indication
+  digitalWrite(greenLed, LOW);
+  digitalWrite(redLed, LOW);
+}
+
+void loop() {
+  fsm.update();
+}
+
+
+
---
+++ ground_robot_code_v1.2/IARC7GroundRobot/StateFunctions.ino
@@ -0,0 +1,346 @@
+/*******************************************************************************
+ * 
+ *  Ground Robot Firmware, developed for IARC Mission 7
+ *
+ *  Developed under Contract to IARC Design Committee
+ *  
+ *  Author: Sterling Lewis Peet <sterling.peet@gatech.edu>
+ *  Date:   January 3, 2014
+ * 
+ *  All Rights Reserved, please contact the IARC Competition Committee for
+ *  permissions requests.
+ *
+ ******************************************************************************/
+// Path Differentiator
+// 0 has competition conditions
+// 1 is in an infinity loop
+// 2 is a circle
+// 3 will try to escape the a following object (manual closeness sensor)
+int path = 2;
+// distance between wheels
+int Rbase = ;
+// conditions for run
+int Rcenter = ;
+// Set to true to force only the maximum noise rather than random
+const boolean MAX_NOISE_TEST = false;
+
+const byte randomMax = 64;
+// Time between trajectory noise injections
+unsigned int noiseInterval = 5000;
+unsigned long lastNoise = 0;
+// Time between auto-reverse
+unsigned int reverseInterval = 20000;
+unsigned long lastReverse = 0;
+// Time needed to affect trajectory
+unsigned int noiseLength = 850;
+unsigned long beginNoise = 0;
+// Time needed to reverse trajectory
+unsigned int reverseLength = 2456; // .33/2 m/s * pi * wheelbase / 2
+unsigned long beginReverse = 0;
+// Time needed to spin 45 degrees
+unsigned int topTouchTime = reverseLength/4;
+unsigned long beginTopTouch = 0;
+boolean isManuevering = false;
+
+////////////////////////////////////////////////////////////////////////////////
+// State Machine Functions for Entering a State
+
+void obsWaitStart()
+{
+  Serial.println("State Change: ObstacleWait");
+  coiPassiveMode();
+  digitalWrite(redLed, HIGH);
+}
+
+void obsRunStart()
+{
+  Serial.println("State Change: ObstacleRun");
+  coiSafeMode();
+  digitalWrite(redLed, HIGH);
+  digitalWrite(greenLed, LOW);
+  // This 9mm/s offset get us close to 5m radius circle trajectory
+  coiDriveDirect(robotSpeed - 9, robotSpeed + 9);
+}
+
+void obsCrashStart()
+{
+  Serial.println("State Change: ObstacleCollision");
+  coiStopMoving();
+  digitalWrite(redLed, HIGH);
+  digitalWrite(greenLed, HIGH);
+}
+
+void trgtWaitStart()
+{
+  Serial.println("State Change: TargetWait");
+  digitalWrite(greenLed, HIGH);
+  coiPassiveMode();
+}
+
+void trgtRunStart()
+{
+  Serial.println("State Change: TargetRun");
+  coiSafeMode();
+  coiDriveDirect(robotSpeed, robotSpeed);
+  digitalWrite(greenLed, HIGH);
+  digitalWrite(redLed, LOW);
+  //lastNoise = millis();
+  //lastReverse = millis();
+}
+// new
+void circRunStart()
+{
+  Serial.println("State Change: TargetRun");
+  coiSafeMode();
+  coiDriveDirect(robotSpeed*(Rcenter-.5*Rbase)/(Rcenter+.5*Rbase), robotSpeed);
+  digitalWrite(greenLed, HIGH);
+  digitalWrite(redLed, LOW);
+  //lastNoise = millis();
+  //lastReverse = millis();
+}
+
+void vNoiseStart()
+{
+  Serial.println("State Change: TrajectoryNoise");
+  long rand = random(randomMax);
+  int offset = (int) rand - randomMax / 2;
+  if(MAX_NOISE_TEST)
+    offset = 127; // force the maximum noise for testing
+  coiDriveDirect(robotSpeed - offset, robotSpeed + offset);
+  digitalWrite(redLed, HIGH);
+  beginNoise = millis();
+}
+
+void vReverseStart()
+{
+  Serial.println("State Change: Reverse");
+  coiDriveDirect(-robotSpeed/2, robotSpeed/2);
+  digitalWrite(redLed, HIGH);
+  beginReverse = millis();
+}
+
+void trgtCrashStart()
+{
+  Serial.println("State Change: TargetCollision");
+  coiStopMoving();
+}
+
+void touchStart()
+{
+  Serial.println("State Change: TopTouch");
+  coiDriveDirect(-robotSpeed/2, robotSpeed/2);
+  digitalWrite(redLed, HIGH);
+  beginTopTouch = millis();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// State Machine Functions for Updating a State
+
+void obsWait()
+{
+  if(isRunSig())
+  {
+    fsm.transitionTo(ObstacleRun);
+  }
+}
+
+void obsRun()
+{
+  if(isWaitSig())
+  {
+    fsm.transitionTo(ObstacleWait);
+  }
+  else
+  {
+    if(coiCheckBump() != 0)
+    {
+      fsm.transitionTo(ObstacleCollision);
+    } 
+    else
+    {
+      delay(15);
+    }
+  }
+}
+
+void obsCrash()
+{
+  if(isWaitSig())
+  {
+    fsm.transitionTo(ObstacleWait);
+  } 
+  else
+  {
+    byte bump = coiCheckBump();
+    if(bump == 0)
+    {
+      fsm.transitionTo(ObstacleRun);
+    } 
+    else
+    {
+      delay(15);
+    }
+  }
+}
+
+void trgtWait()
+{
+  if(isRunSig() && path == 0)
+  {
+    fsm.transitionTo(TargetRun);
+  }
+  else 
+  {
+    fsm.transitionTo(CircleRun);
+  }
+}
+
+void trgtRun()
+{
+  if(isWaitSig())
+  {
+    fsm.transitionTo(TargetWait);
+  } 
+  else if (isTopTouch())
+  {
+    fsm.transitionTo(TopTouch);
+  } 
+  else if (isTimeUp(&lastReverse, &reverseInterval))
+  {
+    fsm.transitionTo(Reverse);
+  }
+  else if (isTimeUp(&lastNoise, &noiseInterval))
+  {
+    fsm.transitionTo(TrajectoryNoise);
+  }
+  else
+  {
+    if(coiCheckBump() != 0 || digitalRead(runSigPin))
+    {
+      fsm.transitionTo(TargetCollision);
+    } 
+    else
+    {
+      // Manuel Control
+      // seemed immediatly revert back to orginal path unless the button was held
+      // Next experiment
+      // Hold in lap but DO NOT COMPRESS BUMBPER
+//      if(!isManuevering && digitalRead(runSigPin))
+//      {
+//        coiDriveDirect(robotSpeed, 0);
+//        isManuevering = true;
+//      }
+//      else if (isManuevering && digitalRead(runSigPin))
+//      {
+//        coiDriveDirect(robotSpeed, robotSpeed);
+//        isManuevering = false;
+//      }
+      delay(15);
+    }
+  }
+}
+
+void circRun()
+{
+  if(isWaitSig())
+  {
+    fsm.transitionTo(TargetWait);
+  } 
+  else
+  {
+    if(coiCheckBump() != 0 )
+    {
+      fsm.transitionTo(TargetCollision);
+    } 
+    else
+    {
+      delay(15);
+    }
+  }
+}
+
+
+void vNoise()
+{
+  if(isWaitSig())
+  {
+    fsm.transitionTo(TargetWait);
+  }  
+  else if (isTopTouch())
+  {
+    fsm.transitionTo(TopTouch);
+  }  
+  else if (isTimeUp(&beginNoise, &noiseLength))
+  {
+    fsm.transitionTo(TargetRun);
+  }
+  else
+  {
+    if(coiCheckBump() != 0)
+    {
+      fsm.transitionTo(TargetCollision);
+    } 
+    else
+    {
+      delay(15);
+    }
+  }
+}
+
+void vReverse()
+{
+  if(isWaitSig())
+  {
+    fsm.transitionTo(TargetWait);
+  }  
+  else if (isTopTouch())
+  {
+    fsm.transitionTo(TopTouch);
+  } 
+  else if (isTimeUp(&beginReverse, &reverseLength))
+  {
+    fsm.transitionTo(TargetRun);
+  }
+}
+
+void trgtCrash()
+{
+  fsm.transitionTo(Reverse);
+}
+
+void touch()
+{
+  if(isWaitSig())
+  {
+    fsm.transitionTo(TargetWait);
+  }  
+  else if (isTimeUp(&beginTopTouch, &topTouchTime))
+  {
+    fsm.transitionTo(TargetRun);
+  }
+  else
+  {
+    if(coiCheckBump() != 0)
+    {
+      fsm.transitionTo(TargetCollision);
+    } 
+    else
+    {
+      delay(15);
+    }
+  }  
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// State Machine Functions for Exiting a State
+
+void trgtWaitExit()
+{
+  Serial.println("Resetting Noise and Reverse timers");
+  lastNoise = millis();
+  lastReverse = millis();
+}
+
+void nullFunc(){ /* Do Nothing, but act as a placeholder */
+}
+
